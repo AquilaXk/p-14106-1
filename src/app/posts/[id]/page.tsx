7feeab9d366d522b5a7c4c411395070392fa0c84
AquilaXk <illusiveman7@gmail.com>
2025-12-11 15:23:12 +0900

    step ~34

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -25,6 +25,52 @@ export default function Page() {
     });
   };
 
+  const deleteComment = (id: number, commentId: number) => {
+    apiFetch(`/api/v1/posts/${id}/comments/${commentId}`, {
+      method: "DELETE",
+    }).then((data) => {
+      alert(data.msg);
+
+      if (postComments == null) return;
+
+      setPostComments(postComments.filter((c) => c.id != commentId));
+    });
+  };
+
+  const handleCommentWriteFormSubmit = (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+
+    const contentTextarea = form.elements.namedItem(
+      "content"
+    ) as HTMLTextAreaElement;
+
+    contentTextarea.value = contentTextarea.value.trim();
+
+    if (contentTextarea.value.length === 0) {
+      alert("댓글 내용을 입력해주세요.");
+      contentTextarea.focus();
+      return;
+    }
+
+    apiFetch(`/api/v1/posts/${id}/comments`, {
+      method: "POST",
+      body: JSON.stringify({
+        content: contentTextarea.value,
+      }),
+    }).then((data) => {
+      alert(data.msg);
+      contentTextarea.value = "";
+
+      if (postComments == null) return;
+
+      setPostComments([...postComments, data.data]);
+    });
+  };
+
   useEffect(() => {
     apiFetch(`/api/v1/posts/${id}`)
       .then(setPost);
@@ -53,6 +99,20 @@ export default function Page() {
           수정
         </Link>
       </div>
+
+      <h2>댓글 작성</h2>
+
+      <form className="p-2" onSubmit={handleCommentWriteFormSubmit}>
+        <textarea
+          className="border p-2 rounded"
+          name="content"
+          placeholder="댓글 내용"
+        />
+        <button className="p-2 rounded border" type="submit">
+          작성
+        </button>
+      </form>
+
       <h2>댓글 목록</h2>
 
       {postComments == null && <div>댓글 로딩중...</div>}
@@ -64,7 +124,18 @@ export default function Page() {
       {postComments != null && postComments.length > 0 && (
         <ul>
           {postComments.map((comment) => (
-            <li key={comment.id}>{comment.content}</li>
+            <li key={comment.id}>
+              {comment.id} : {comment.content}
+              <button
+                className="p-2 rounded border"
+                onClick={() =>
+                  confirm(`${comment.id}번 댓글을 정말로 삭제하시겠습니까?`) &&
+                  deleteComment(id, comment.id)
+                }
+              >
+                삭제
+              </button>
+            </li>
           ))}
         </ul>
       )}
