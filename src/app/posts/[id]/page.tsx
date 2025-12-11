27bc12e5b4f9f00a5494a4e44addba79701e5f22
AquilaXk <illusiveman7@gmail.com>
2025-12-11 14:38:58 +0900

    step ~30

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type { PostWithContentDto } from "@/type/post";
+import type { PostCommentDto, PostWithContentDto } from "@/type/post";
 import { apiFetch } from "@/lib/backend/client";
 import { use, useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
@@ -14,6 +14,7 @@ export default function Page() {
   const router = useRouter();
 
   const [post, setPost] = useState<PostWithContentDto | null>(null);
+  const [postComments, setPostComments] = useState<PostCommentDto[] | null>([]);
 
   const deletePost = (id: number) => {
     apiFetch(`/api/v1/posts/${id}`, {
@@ -27,6 +28,8 @@ export default function Page() {
   useEffect(() => {
     apiFetch(`/api/v1/posts/${id}`)
       .then(setPost);
+    apiFetch(`/api/v1/posts/${id}/comments`)
+      .then(setPostComments);
   }, []);
 
   if (post == null) return <div className="ml-2">로딩 중 . . .</div>;
@@ -50,6 +53,21 @@ export default function Page() {
           수정
         </Link>
       </div>
+      <h2>댓글 목록</h2>
+
+      {postComments == null && <div>댓글 로딩중...</div>}
+
+      {postComments != null && postComments.length == 0 && (
+        <div>댓글이 없습니다.</div>
+      )}
+
+      {postComments != null && postComments.length > 0 && (
+        <ul>
+          {postComments.map((comment) => (
+            <li key={comment.id}>{comment.content}</li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
